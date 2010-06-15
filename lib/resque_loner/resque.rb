81cedcc59e8fd2b076e813a38f5ca18c4a6c4469
Jannis Hermanns <jannis@moviepilot.de>
2010-06-15 16:26:25 +0200

    Green specs, now refactor

--- lib/resque_loner/resque.rb
+++ lib/resque_loner/resque.rb
@@ -6,30 +6,45 @@ module ResqueLoner
     end
     
     module ClassMethods
-
+      
       def push(queue, item)
         return "EXISTED" if loner_queued?(queue, item)
         if super(queue, item) == "OK"
           mark_loner_as_queued(queue, item)
         end
       end
+      
+      def pop(queue)
+        item = super(queue)
+        mark_loner_as_unqueued queue, item
+        return item
+      end
 
       def loner_queued?(queue, item)
-        klass = constantize(item[:class])
-        return false unless klass.ancestors.include?(::Resque::LonerJob)
-        redis.get(loner_job_queue_key(queue, klass.redis_key(item[:args]))) == "1"
+        return false unless item_is_a_loner_job?(item)
+        redis.get(loner_job_queue_key(queue, item)) == "1"
       end
       
       def mark_loner_as_queued(queue, item)
-        klass = constantize(item[:class])
-        return false unless klass.ancestors.include?(::Resque::LonerJob)
-        debugger
-        redis.set(loner_job_queue_key(queue, klass.redis_key(item[:args])), 1)
+        return unless item_is_a_loner_job?(item)
+        redis.set(loner_job_queue_key(queue, item), 1)
       end
 
-      def loner_job_queue_key(queue, job_key)
+      def mark_loner_as_unqueued(queue, item)
+        return unless item_is_a_loner_job?(item)
+        redis.del(loner_job_queue_key(queue, item))
+      end
+
+      def loner_job_queue_key(queue, item)
+        job_key = constantize(item[:class] || item["class"]).redis_key(item[:args] || item["args"])
         "queue:#{queue}:loners:#{job_key}"
       end
+      
+      def item_is_a_loner_job?(item)
+        klass = constantize(item[:class] || item["class"])
+        klass.ancestors.include?(::Resque::LonerJob)
+      end
+
     end
   end
 end
--- spec/loner_spec.rb
+++ spec/loner_spec.rb
@@ -6,6 +6,8 @@ end
 
 class SomeLonerJob < Resque::LonerJob
   @queue = :other_queue
+  def self.perform
+  end
 end
 
 describe "Resque" do
@@ -22,6 +24,17 @@ describe "Resque" do
   
   it "only one of the same job sits in a queue" do
     Resque.enqueue SomeLonerJob, "foo"
+    Resque.enqueue SomeLonerJob, "foo"
+    Resque.size(:other_queue).should == 1
+  end
+  
+  it "should allow the same jobs to be executed one after the other" do
+    Resque.enqueue SomeLonerJob, "foo"
+    Resque.size(:other_queue).should == 1
+
+    Resque.pop(:other_queue)
+    Resque.size(:other_queue).should == 0
+    
     Resque.enqueue SomeLonerJob, "foo"
     Resque.size(:other_queue).should == 1
   end
