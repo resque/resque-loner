593e0f54dc98ee52d07e9c5748d90c6806cca118
Jannis Hermanns <jannis@moviepilot.de>
2010-06-15 18:33:31 +0200

    Cleanup

--- lib/resque_loner/job.rb
+++ lib/resque_loner/job.rb
@@ -1,46 +1,53 @@
+#
+#  Since there were not enough hooks to hook into, I have to overwrite
+#  3 methods of Resque::Job - the rest of the implementation is in the
+#  proper Plugin namespace.
+# 
 module Resque
-#  module Plugins
-#    module Loner
-      class Job
-        
-        
-        #
-        #  Overwriting original create method to mark an item as queued
-        #  after Resque::Job.create has called Resque.push
-        #
-        def self.create_with_loner(queue, klass, *args)
-          item = { :class => klass.to_s, :args => args }
-          return "EXISTED" if Resque::Plugins::Loner::Helpers.loner_queued?(queue, item)
-          create_without_loner(queue, klass, *args)
-          Resque::Plugins::Loner::Helpers.mark_loner_as_queued(queue, item)
-        end
-        
-        #
-        #  Overwriting original reserve method to mark an item as unqueued
-        #
-        def self.reserve_with_loner(queue)
-          item = reserve_without_loner(queue)
-          Resque::Plugins::Loner::Helpers.mark_loner_as_unqueued( queue, item ) if item
-          item
-        end
-        
-        #
-        #  Overwriting original destroy method to mark all destroyed jobs as unqueued
-        #
-        def self.destroy_with_loner(queue, klass, *args)
-          Resque::Plugins::Loner::Helpers.job_destroy(queue, klass, *args)
-          destroy_without_loner(queue, klass, *args)
-        end
-        
-        class << self
-          alias_method :create_without_loner, :create
-          alias_method :create, :create_with_loner
-          alias_method :reserve_without_loner, :reserve
-          alias_method :reserve, :reserve_with_loner
-          alias_method :destroy_without_loner, :destroy
-          alias_method :destroy, :destroy_with_loner
-        end
-      end
-#    end
-#  end
+  class Job
+    
+    
+    #
+    #  Overwriting original create method to mark an item as queued
+    #  after Resque::Job.create has called Resque.push
+    #
+    def self.create_with_loner(queue, klass, *args)
+      item = { :class => klass.to_s, :args => args }
+      return "EXISTED" if Resque::Plugins::Loner::Helpers.loner_queued?(queue, item)
+      create_without_loner(queue, klass, *args)
+      Resque::Plugins::Loner::Helpers.mark_loner_as_queued(queue, item)
+    end
+    
+    #
+    #  Overwriting original reserve method to mark an item as unqueued
+    #
+    def self.reserve_with_loner(queue)
+      item = reserve_without_loner(queue)
+      Resque::Plugins::Loner::Helpers.mark_loner_as_unqueued( queue, item ) if item
+      item
+    end
+    
+    #
+    #  Overwriting original destroy method to mark all destroyed jobs as unqueued.
+    #  Because the original method only returns the amount of jobs destroyed, but not 
+    #  the jobs themselves. Hence Resque::Plugins::Loner::Helpers.job_destroy looks almost
+    #  as the original method Resque::Job.destroy. Couldn't make it any dry'er.
+    #
+    def self.destroy_with_loner(queue, klass, *args)
+      Resque::Plugins::Loner::Helpers.job_destroy(queue, klass, *args)
+      destroy_without_loner(queue, klass, *args)
+    end
+    
+    #
+    # Chain..
+    #
+    class << self
+      alias_method :create_without_loner, :create
+      alias_method :create, :create_with_loner
+      alias_method :reserve_without_loner, :reserve
+      alias_method :reserve, :reserve_with_loner
+      alias_method :destroy_without_loner, :destroy
+      alias_method :destroy, :destroy_with_loner
+    end
+  end
 end
--- lib/resque_loner/unique_job.rb
+++ lib/resque_loner/unique_job.rb
@@ -1,15 +1,23 @@
 require 'digest/md5'
 
+#
+#  If you want your job to be unique, subclass it from this class. If you wish,
+#  you can overwrite this implementation of redis_key to fit your needs
+#
 module Resque
   module Plugins
     module Loner
       class UniqueJob
         extend Resque::Helpers
 
-        def self.redis_key(item)
-          item = decode(encode(item)) # This is the cycle the data goes when being enqueued/dequeued
-          job  = item[:class] || item["class"]
-          args = item[:args]  || item["args"]
+        #
+        #  Payload is what Resque stored for this job along with the job's class name.
+        #  On a Resque with no plugins installed, this is a hash containing :class and :args
+        #
+        def self.redis_key(payload)
+          payload = decode(encode(payload)) # This is the cycle the data goes when being enqueued/dequeued
+          job  = payload[:class] || payload["class"]
+          args = payload[:args]  || payload["args"]
           digest = Digest::MD5.hexdigest encode(:class => job, :args => args)
           digest
         end
